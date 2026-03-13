b75b61db4027ed814ea209a11dc71ac12522c1f0
Shahanur Alam <108014474+shahanuralamofficial@users.noreply.github.com>
2026-03-13 14:40:16 +0600

    Configure release signing for Android app
    - Implement logic to load signing configuration from `key.properties`
    - Define a `release` signing configuration using properties for `keyAlias`, `keyPassword`, `storeFile`, and `storePassword`
    - Update the release build type to use the new `release` signing configuration instead of the `debug` one

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,16 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
 }
 
 android {
@@ -28,9 +36,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
